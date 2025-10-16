1928bc6af0a428333c7fc449d2134f9c0cf12aa7
Sunam kundal <150831772+sunamkundal01@users.noreply.github.com>
2025-10-16 11:38:46 +0530

    Update AI model version to gemini-2.5-flashed because the 1.5 model is no longer exists in gemini

--- configs/AIModel.js
+++ configs/AIModel.js
@@ -8,8 +8,9 @@ const {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
-  });
+  model: "gemini-2.5-flash",
+});
+
   
   const generationConfig = {
     temperature: 1,
